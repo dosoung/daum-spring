f9379d51933d2df9463145b8fc5cf7cd16188d9e
dosoung <soungdo0919@gmail.com>
2020-06-24 02:08:02 +0900

    make project and DB

---
+++ post/src/test/java/jeju/ac/kr/post/PostApplicationTests.java
@@ -0,0 +1,13 @@
+package jeju.ac.kr.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
