566e77fcfe20749a502d7ad4cfe3db0383c454df
dosoung <soungdo0919@gmail.com>
2020-06-24 04:00:02 +0900

    Finish Controller

--- post/src/main/java/jeju/ac/kr/post/BoardController.java
+++ post/src/main/java/jeju/ac/kr/post/BoardController.java
@@ -50,7 +50,36 @@ public class BoardController {
         return "redirect:/boards";
     }
 
-//    @GetMapping("/update/teacher")
+    @GetMapping("update/board/{id}")
+    public String updatePost(@PathVariable("id") int id, ModelMap boardModel) {
+        boardModel.addAttribute("id",id);
+        Board board = boardService.getBoard(id);
+        boardModel.addAttribute("board",board);
+        return "update";
+    }
+
+    @PostMapping("/update/board")
+    public String updateBoard(@RequestParam int id, @RequestParam(value="name", required=true) String name,
+                              @RequestParam(value="phone" ,required=true) String phone,
+                              @RequestParam(value="email", required=true) String email, ModelMap boardModel) {
+        Board board = new Board(id,name,phone,email);
+
+        boardService.updateBoard(board);
+        List<Board> boards = boardService.getBoards();
+        boardModel.addAttribute("boards",boards);
+        boardModel.addAttribute("id",id);
+        boardModel.addAttribute("message","게시판 수정이 완료 되었습니다.");
+        return "redirect:/boards";
+    }
+
+    @GetMapping("/delete/board/{id}")
+    public String deleteBoard(@PathVariable int id, ModelMap boardModel) {
+        boardService.deleteBoard(id);
+        List<Board> boards = boardService.getBoards();
+        boardModel.addAttribute("boards",boards);
+        boardModel.addAttribute("messamge","삭제가 완료 되었습니다.");
+        return "redirect:/boards";
+    }
 
 
 
