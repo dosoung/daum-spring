10928eebe1a602fb3d6bbb6c9dd61fea78eec25c
dosoung <soungdo0919@gmail.com>
2020-06-24 02:23:27 +0900

    DaoSource

---
+++ post/src/main/java/jeju/ac/kr/post/Board.java
@@ -0,0 +1,18 @@
+package jeju.ac.kr.post;
+
+public class Board {
+
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+
+    public Board(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+
+    }
+}
